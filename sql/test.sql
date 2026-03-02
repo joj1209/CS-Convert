3c8343f2827bb5d8948e2dae760e7603acc6541a
joj1209 <joj1209@naver.com>
2026-03-02 21:56:19 +0900

    chore: update test SQL

--- sql/test.sql
+++ sql/test.sql
@@ -8,3 +8,9 @@ BEGIN
     DECLARE vs_tbl_nm STRING DEFAULT {vs_tbl_nm};
 
 ENd;
+
+
+
+    DECLARE vs_job_d STRING DEFAULT @standard_date;
+    DECLARE vs_pgm_id STRING DEFAULT @program_id;
+    DECLARE vs_tbl_nm STRING DEFAULT @table_name;
